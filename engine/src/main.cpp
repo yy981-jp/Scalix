123d8b576a6c7ac7f62771aa361977332b2aa25e
yy981-jp <134594870+yy981-jp@users.noreply.github.com>
2026-05-28 11:40:20 +0900

    test init

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,14 +1,19 @@
 #define SDL_MAIN_HANDLED
 
-#include "core/game.h"
+// #include "core/game.h"
 
-int main() {
-	{
-		Game game;
 
-		while (game.isRunning()) {
-			game.tick();
-		}
-	}
-	bgfx::shutdown();
-}
+// int main() {
+	// 	{
+		// 		Game game;
+
+// 		while (game.isRunning()) {
+// 			game.tick();
+// 		}
+// 	}
+// 	bgfx::shutdown();
+// }
+
+
+
+#include "../../test/sd.h"
