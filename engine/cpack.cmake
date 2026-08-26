87af3784cf2f91a262c85b56112367f8f570e77f
yy981-jp <134594870+yy981-jp@users.noreply.github.com>
2026-08-26 23:26:12 +0900

    wip: feat: dist cpack

--- engine/cpack.cmake
+++ engine/cpack.cmake
@@ -1,3 +1,62 @@
+get_filename_component(MSYS_BIN_DIR "${CMAKE_CXX_COMPILER}" DIRECTORY)
+
+
+# wildcard_pattern : DLLファイル名にマッチさせるワイルドカードパターン (例: "libwebp-*.dll")
+# search_dir        : DLLを探索するディレクトリ(ビルド出力先など)
+# dest_dir          : インストール先(CPackのパッケージに入るパス)
+function(install_dlls_by_wildcard wildcard_pattern search_dir dest_dir)
+    install(CODE "
+		cmake_policy(SET CMP0207 NEW)
+
+        set(_pattern \"${wildcard_pattern}\")
+        set(_search_dir \"${search_dir}\")
+        set(_dest_dir \"${dest_dir}\")
+
+        # ワイルドカードで直接マッチするDLLを収集(正規表現不要)
+        file(GLOB_RECURSE _matched_dlls \"\${_search_dir}/\${_pattern}\")
+
+        if(_matched_dlls)
+            # message(WARNING \"Matched DLLs: \${_matched_dlls}\")
+
+            # 依存先を再帰的に解決
+            file(GET_RUNTIME_DEPENDENCIES
+                LIBRARIES \${_matched_dlls}
+                RESOLVED_DEPENDENCIES_VAR _resolved
+                UNRESOLVED_DEPENDENCIES_VAR _unresolved
+                DIRECTORIES \${_search_dir} \ ${MSYS_BIN_DIR}
+                PRE_EXCLUDE_REGEXES \"api-ms-\" \"ext-ms-\"
+                POST_EXCLUDE_REGEXES \".*[Ss]ystem32.*\"
+            )
+
+			# message(WARNING \"DEST DIR: \${_dest_dir}\")
+			# message(WARNING \"MATCHED: \${_matched_dlls}\")
+			# message(WARNING \"CMAKE_INSTALL_PREFIX: \${CMAKE_INSTALL_PREFIX}\")
+
+			# マッチしたDLL本体をインストール
+			file(INSTALL
+				DESTINATION \"\${CMAKE_INSTALL_PREFIX}/\${_dest_dir}\"
+				TYPE SHARED_LIBRARY
+				FILES \${_matched_dlls}
+			)
+	            # 依存先DLLもインストール
+            if(_resolved)
+				file(INSTALL
+					DESTINATION \"\${CMAKE_INSTALL_PREFIX}/\${_dest_dir}\"
+					TYPE SHARED_LIBRARY
+					FILES \${_resolved}
+				)
+            endif()
+
+            if(_unresolved)
+                message(WARNING \"解決できなかった依存: \${_unresolved}\")
+            endif()
+        else()
+            message(WARNING \"パターン '\${_pattern}' にマッチするDLLが見つかりませんでした\")
+        endif()
+    " ALL_COMPONENTS)
+endfunction()
+
+
 if(WIN32)
 	set(_rd_pre_exclude "api-ms-" "ext-ms-")
 	set(_rd_post_exclude ".*system32/.*\\.dll")
@@ -13,13 +72,26 @@ install(
 	TARGETS ${EXE_NAME}
 	RUNTIME_DEPENDENCIES
 		DIRECTORIES
-			${CMAKE_PREFIX_PATH}/bin
-			"C:/msys64/mingw64/bin"
+			# ${CMAKE_PREFIX_PATH}/bin
+			${MSYS_BIN_DIR}
+			# "C:/msys64/mingw64/bin"
 		PRE_EXCLUDE_REGEXES ${_rd_pre_exclude}
 		POST_EXCLUDE_REGEXES ${_rd_post_exclude}
 		RUNTIME DESTINATION bin
 	RUNTIME DESTINATION bin
 )
+
+# message(WARNING ${MSYS_BIN_DIR})
+
+# file(GET_RUNTIME_DEPENDENCIES
+#     LIBRARIES "${CMAKE_PREFIX_PATH}/bin/libwebp-*"           # 依存関係を調べたい対象のDLLパス
+#     DIRECTORIES "${CMAKE_PREFIX_PATH}/bin"      # DLLを探す検索ディレクトリのリスト
+#     RESOLVED_DEPENDENCIES_VAR resolved_deps   # 解決された依存DLLのパスが入る変数
+#     UNRESOLVED_DEPENDENCIES_VAR unresolved_deps # 未解決の依存DLLが入る変数
+# )
+
+
+install_dlls_by_wildcard("libwebp*" "${MSYS_BIN_DIR}" "bin")
 
 # shaders
 install(
